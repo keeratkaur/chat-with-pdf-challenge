b38b42525e0eed0852b4143f09df3f2db0cdd507
Harkirat Kaur <kkirat911@gmail.com>
2025-06-03 12:52:25 -0230

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
+///video/jshfhdfhdsik/analysis protected page
+// if the person is not signed and tries to access the features, then we need to redirect the user to the signin; make a middleware
+
 const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
