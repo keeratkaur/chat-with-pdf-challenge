1b20552fa1211b1520c587e5994c6ede26601a68
Harkirat Kaur <kkirat911@gmail.com>
2025-06-15 00:22:18 -0230

    Add ClientWrapper component and update package dependencies

---
+++ app/ClientWrapper.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { SchematicProvider } from "@schematichq/schematic-react";
+
+export default function ClientWrapper({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return <ClerkProvider>
+        <SchematicProvider>
+            {children}
+        </SchematicProvider>
+        </ClerkProvider>;
+}
